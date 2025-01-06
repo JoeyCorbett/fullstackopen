96916dec31c4f006bf54f4266b1480d25969e3b5
JoeyCorbett <corbettjoey9@gmail.com>
2025-01-05 23:04:32 -0500

    Complete 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,21 +7,24 @@ const Statistics = ({ good, neutral, bad }) => {
   if (total === 0) return <div>No feedback given</div>;
 
   return (
-    <>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={total} />
-      <StatisticLine text="average" value={(good - bad) / total} />
-      <StatisticLine text="positive" value={(good / total) * 100 + "%"} />
-    </>
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={total} />
+        <StatisticLine text="average" value={(good - bad) / total} />
+        <StatisticLine text="positive" value={(good / total) * 100 + "%"} />
+      </tbody>
+    </table>
   );
 };
 
 const StatisticLine = ({ text, value }) => (
-  <div>
-    {text} {value}
-  </div>
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
 );
 
 const App = () => {
@@ -30,14 +33,14 @@ const App = () => {
   const [bad, setBad] = useState(0);
 
   return (
-    <div>
+    <>
       <h2>give feedback</h2>
       <Button onClick={() => setGood(good + 1)} text="good" />
       <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button onClick={() => setBad(bad + 1)} text="bad" />
       <h1>statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad} />
-    </div>
+    </>
   );
 };
 
