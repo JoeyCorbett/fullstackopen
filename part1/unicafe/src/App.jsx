772e26c3d3b294a9bd2744e515f0d726c14e55ca
JoeyCorbett <corbettjoey9@gmail.com>
2025-01-05 22:48:08 -0500

    Complete 1.10

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,19 +4,25 @@ const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
 const Statistics = ({ good, neutral, bad }) => {
   let total = good + neutral + bad;
-  if (total === 0) return <div>No feedback given</div>
-  
+  if (total === 0) return <div>No feedback given</div>;
+
   return (
     <>
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>all {total}</div>
-      <div>average {(good - bad) / total}</div>
-      <div>positive {(good / total) * 100} %</div>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={total} />
+      <StatisticLine text="average" value={(good - bad) / total} />
+      <StatisticLine text="positive" value={(good / total) * 100 + "%"} />
     </>
   );
 };
+
+const StatisticLine = ({ text, value }) => (
+  <div>
+    {text} {value}
+  </div>
+);
 
 const App = () => {
   const [good, setGood] = useState(0);
