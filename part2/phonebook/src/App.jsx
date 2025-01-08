ad697fa773ef5a01644d20e9adc22ffb4e1c643f
JoeyCorbett <corbettjoey9@gmail.com>
2025-01-08 14:04:27 -0500

    Complete 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,40 +1,46 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+    { name: "Arto Hellas", number: '040-1234567' }
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("")
 
   const addNote = (e) => {
-    e.preventDefault()
-    if (persons.some(person => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`)
+    e.preventDefault();
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
     } else {
-      const noteObject = { name: newName }
-      setPersons(persons.concat(noteObject))
-      setNewName('')
+      const noteObject = { name: newName, number: newNumber };
+      setPersons(persons.concat(noteObject));
+      setNewName("");
+      setNewNumber("");
     }
-  }
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addNote}>
         <div>
-          name: <input 
-          value={newName}
-          onChange={e => setNewName(e.target.value)} />
+          name:{" "}
+          <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+        </div>
+        <div>
+          number:{" "}
+          <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => 
-      <div key={person.name}>{person.name}</div>)}
+      {persons.map((person) => (
+        <div key={person.name}>{person.name} {person.number}</div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
