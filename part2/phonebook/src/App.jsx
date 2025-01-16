d05148c63c6ba5d44a6b62100fb22b209c752b3a
JoeyCorbett <corbettjoey9@gmail.com>
2025-01-15 20:01:59 -0500

    Fix formatting

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,55 +1,55 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "react"
 
-import Filter from "./components/Filter";
-import PersonForm from "./components/PersonForm";
-import Persons from "./components/Persons";
-import Notification from "./components/Notification";
+import Filter from "./components/Filter"
+import PersonForm from "./components/PersonForm"
+import Persons from "./components/Persons"
+import Notification from "./components/Notification"
 
-import contactService from "./services/contacts";
+import contactService from "./services/contacts"
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
-  const [search, setSearch] = useState("");
-  const [info, setInfo] = useState({ message: null });
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState("")
+  const [newNumber, setNewNumber] = useState("")
+  const [search, setSearch] = useState("")
+  const [info, setInfo] = useState({ message: null })
 
   useEffect(() => {
     contactService.getAll().then((initialContacts) => {
-      setPersons(initialContacts);
-    });
-  }, []);
+      setPersons(initialContacts)
+    })
+  }, [])
 
   const notifyWith = (message, type = "info") => {
     setInfo({
       message,
       type,
-    });
+    })
 
     setTimeout(() => {
-      setInfo({ message: null });
-    }, 3000);
-  };
+      setInfo({ message: null })
+    }, 3000)
+  }
 
   const cleanForm = () => {
-    setNewName('');
-    setNewNumber('');
+    setNewName('')
+    setNewNumber('')
   }
 
   const findPerson = (name) => {
     return persons.find(
       (person) => person.name.toLowerCase() === name.toLowerCase()
-    );
-  };
+    )
+  }
 
   const confirmUpdate = (personName) => {
     return window.confirm(
       `${personName} is already added to phonebook, replace the old number with a new one?`
-    );
-  };
+    )
+  }
 
   const updateContact = (personObj) => {
-    const noteObject = { ...personObj, number: newNumber };
+    const noteObject = { ...personObj, number: newNumber }
 
     contactService
       .update(personObj.id, noteObject)
@@ -58,60 +58,60 @@ const App = () => {
           persons.map((person) =>
             person.id === personObj.id ? returnedPerson : person
           )
-        );
+        )
         notifyWith(
           `Update ${returnedPerson.name}'s number to ${returnedPerson.number}`
-        );
+        )
       })
       .catch(() => {
         notifyWith(
           `Information of ${personObj.name} has already been removed from server`,
           "error"
-        );
-      });
+        )
+      })
       cleanForm()
-  };
+  }
 
   const addNewContact = () => {
-    const noteObject = { name: newName, number: newNumber };
+    const noteObject = { name: newName, number: newNumber }
     contactService
       .create(noteObject)
       .then((returnedContact) => {
-      setPersons(persons.concat(returnedContact));
-      notifyWith(`Added ${returnedContact.name}`);
+      setPersons(persons.concat(returnedContact))
+      notifyWith(`Added ${returnedContact.name}`)
       cleanForm()
       })
       .catch(error => {
         notifyWith(error.response.data.error, "error")
       })
-  };
+  }
 
   const addNote = (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
-    const personObj = findPerson(newName);
+    const personObj = findPerson(newName)
     if (personObj) {
       if (confirmUpdate(personObj.name)) {
-        updateContact(personObj);
+        updateContact(personObj)
       }
     } else {
-      addNewContact();
+      addNewContact()
     }
-  };
+  }
 
   const deletePerson = (person) => {
     if (confirm(`Delete ${person.name} ?`)) {
       contactService.deleteData(person.id).then(() => {
-        setPersons(persons.filter(p => p.id !== person.id));
+        setPersons(persons.filter(p => p.id !== person.id))
         
         notifyWith(`number of ${person.name} deleted!`)
-      });
+      })
     }
-  };
+  }
 
   const searchResults = persons.filter((person) =>
     person.name.toLowerCase().includes(search.toLowerCase())
-  );
+  )
 
   return (
     <div>
@@ -129,7 +129,7 @@ const App = () => {
       <h2>Numbers</h2>
       <Persons deletePerson={deletePerson} searchResults={searchResults} />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
