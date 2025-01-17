0914f87785fcd57c5ac3c2df1664bb9f1fa48ec4
JoeyCorbett <corbettjoey9@gmail.com>
2025-01-16 19:56:57 -0500

    Complete 4.5

--- part3/blog-list/utils/list_helper.js
+++ part3/blog-list/utils/list_helper.js
@@ -1,4 +1,4 @@
-const dummy = (blogs) => {
+const dummy = () => {
   return 1
 }
 
@@ -6,7 +6,20 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+
+  const maxObj = blogs.reduce(
+    (max, blog) => blog.likes > max.likes ? blog : max, blogs[0]
+  )
+  const { _id, __v, ...result } = maxObj
+  return result
+}
+
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
