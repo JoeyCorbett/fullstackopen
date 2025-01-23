02045af32f2a9039c3c657c0ed8915fde8edc87e
JoeyCorbett <corbettjoey9@gmail.com>
2025-01-23 16:29:34 -0500

    Complete 4.10

--- part3/blog-list/tests/blog_api.test.js
+++ part3/blog-list/tests/blog_api.test.js
@@ -28,10 +28,32 @@ describe('api tests', () => {
     assert.strictEqual(blogs.body.length, 6)
   })
 
-  test.only('blog posts use "id" and not "_id"', async () => {
+  test('blog posts use "id" and not "_id"', async () => {
     const blogsAtStart = await helper.blogsInDb()
 
     assert.strictEqual(Object.keys(blogsAtStart[0])[4], 'id')
+  })
+
+  test.only('a valid note can be added', async () => {
+    const newBlog = {
+      title: 'test note',
+      author: 'test author',
+      url: 'https://example.com',
+      likes: 25,
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    const titles = blogsAtEnd.map(b => b.title)
+
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+
+    assert(titles.includes('test note'))
   })
 })
 
